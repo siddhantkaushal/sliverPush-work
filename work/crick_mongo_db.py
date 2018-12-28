15c893225dbbd6e4c91f612364d706e5c135e3ba
siddhantkaushal <45432348+siddhantkaushal@users.noreply.github.com>
2018-12-28 11:23:12 +0530

    Update crick_mongo_db.py

--- work/crick_mongo_db.py
+++ work/crick_mongo_db.py
@@ -7,7 +7,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["espn"]
 
-mycol = mydb["cric_info"]
+mycol = mydb["cricketers_info"]
 
 list_1 = [i for i in string.ascii_uppercase]
 
@@ -25,7 +25,7 @@ for item in list_1:
     for row in rows:
         res = row.find('td', class_="ciPlayernames")
         name = res.a.text.strip()
-        id = res.a['href'].split('/')[-1].strip('.html')
+        id = int(res.a['href'].split('/')[-1].strip('.html'))
         # print("{}: {} :{}".format(name, id, "espncricinfo"))
 
         if (mycol.find({"id": id}).count() > 0):
